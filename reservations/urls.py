89ddd95d6c13d41c58d4e020a72bcd55e72c7e43
Cédryc Ruth <ceruth@epfc.eu>
2024-10-29 20:37:14 +0100

    Update modèle User +migration +données initiales

--- reservations/urls.py
+++ reservations/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('compte/', include('django.contrib.auth.urls')),
     path('catalogue/', include('catalogue.urls')),
     path('admin/', admin.site.urls),
 ]
