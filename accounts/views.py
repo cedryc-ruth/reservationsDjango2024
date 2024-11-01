8b63796d0eb3a986494df160e0b7493887b394ad
Cédryc Ruth <ceruth@epfc.eu>
2024-11-01 20:33:36 +0100

    Frontend authentication – Basic signup updated

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect
+from django.contrib import messages
 
 #from .forms import SignUpForm
 
@@ -10,10 +11,10 @@ class SignUpView(UserPassesTestMixin, CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-    permission_denied_message = "Vous êtes déjà inscrit!"
 
     def test_func(self):
         return self.request.user.is_anonymous or self.request.user.is_superuser
     
     def handle_no_permission(self):
+        messages.error(self.request, "Vous êtes déjà inscrit!")
         return redirect('home')
