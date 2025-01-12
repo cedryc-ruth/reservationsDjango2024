81c649ffd0af243dcd699f162c7abf69fa87cbf2
Cédryc Ruth <ceruth@epfc.eu>
2025-01-12 17:39:48 +0100

    API RESTful with HATEAOS

--- api/catalogue/serializers.py
+++ api/catalogue/serializers.py
@@ -3,15 +3,15 @@ from catalogue.models import Artist
 from rest_framework.reverse import reverse
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
-    #links = serializers.SerializerMethodField()
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Artist
-        fields = ['id', 'firstname', 'lastname']
+        fields = ['id', 'firstname', 'lastname', 'links']
 
-    #def get_links(self, obj):
-        #request = self.context.get('request')
-        #return {
-        #    'self': reverse('artist-detail', kwargs={'pk': obj.id}, request=request),
-        #    'all_artists': reverse('artist-list', request=request),
-        #}
+    def get_links(self, obj):
+        request = self.context.get('request')
+        return {
+            'self': reverse('artist-detail', kwargs={'pk': obj.id}, request=request),
+            'all_artists': reverse('artist-list', request=request),
+        }
